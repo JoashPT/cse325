32e8897f9e520d660ec215d40525c80fa052a68a
Joash Tantiangco <tan24001@byui.edu>
2025-09-11 13:03:20 +0800

    Initial commit

---
+++ week01HelloWorldTutorial/MyConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {DateTime.Now}");
+
+DateTime christmasDate = new(DateTime.Now.Year, 12, 25);
+TimeSpan daysToChristmas = christmasDate.Subtract(DateTime.Today);
+
+Console.WriteLine($"There are {daysToChristmas.Days} days until Christmas");
