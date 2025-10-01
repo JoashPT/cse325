e935eb3707e9ca1d3583ab45233340fb6a9c4bf0
Joash Tantiangco <tan24001@byui.edu>
2025-10-01 21:30:47 +0800

    changed some files to make it work, apparently I made an error following the tutorial... so blames on me.

--- BlazingPizza2/BlazingPizza/OrderController.cs
+++ BlazingPizza2/BlazingPizza/OrderController.cs
@@ -1,64 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace BlazingPizza;
-
-[Route("orders")]
-[ApiController]
-public class OrdersController : Controller
+namespace BlazingPizza
 {
-    private readonly PizzaStoreContext _db;
-    private OrdersController(PizzaStoreContext db)
+    [Route("orders")]
+    [ApiController]
+    public class OrdersController : Controller
     {
-        _db = db;
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
-    {
-        var orders = await _db.Orders
-        .Include(o => o.Pizzas).ThenInclude(p => p.Special)
-        .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
-        .OrderByDescending(o => o.CreatedTime)
-        .ToListAsync();
-
-        return orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<int>> PlaceOrder(Order order)
-    {
-        order.CreatedTime = DateTime.Now;
-
-        // Enforce existence of Pizza.SpecialId and Topping.ToppingId
-        // in the database - prevent the submitter from making up
-        // new specials and toppings
-        foreach (var pizza in order.Pizzas)
+        private readonly PizzaStoreContext _db;
+        public OrdersController(PizzaStoreContext db)
         {
-            pizza.SpecialId = pizza.Special.Id;
-            pizza.Special = null;
+            _db = db;
         }
 
-        _db.Orders.Attach(order);
-        await _db.SaveChangesAsync();
-
-        return order.OrderId;
-    }
-
-    [HttpGet("{orderId}")]
-    public async Task<ActionResult<OrderWithStatus>> GetOrderWithStatus(int orderId)
-    {
-        var order = await _db.Orders
-            .Where(o => o.OrderId == orderId)
+        [HttpGet]
+        public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
+        {
+            var orders = await _db.Orders
             .Include(o => o.Pizzas).ThenInclude(p => p.Special)
             .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
-            .SingleOrDefaultAsync();
+            .OrderByDescending(o => o.CreatedTime)
+            .ToListAsync();
 
-        if (order == null)
-        {
-            return NotFound();
+            return orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
         }
 
-        return OrderWithStatus.FromOrder(order);
+        [HttpPost]
+        public async Task<ActionResult<int>> PlaceOrder(Order order)
+        {
+            order.CreatedTime = DateTime.Now;
+
+            // Enforce existence of Pizza.SpecialId and Topping.ToppingId
+            // in the database - prevent the submitter from making up
+            // new specials and toppings
+            foreach (var pizza in order.Pizzas)
+            {
+                pizza.SpecialId = pizza.Special.Id;
+                pizza.Special = null;
+            }
+
+            _db.Orders.Attach(order);
+            await _db.SaveChangesAsync();
+
+            return order.OrderId;
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<ActionResult<OrderWithStatus>> GetOrderWithStatus(int orderId)
+        {
+            var order = await _db.Orders
+                .Where(o => o.OrderId == orderId)
+                .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+                .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+                .SingleOrDefaultAsync();
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return OrderWithStatus.FromOrder(order);
+        }
     }
 }
